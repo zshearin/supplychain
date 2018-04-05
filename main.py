100894e08d72db88274d235a9fe049fa6935b1e4
zshearin <zshearin617@gmail.com>
2018-04-05 10:50:44 -0400

    Added changes to main.py, refactored to contain scheduling element in scheduler.py

--- main.py
+++ main.py
@@ -1,317 +1,115 @@
 
-class Time:
+from scheduler import *
+import random 
+import sys
 
-	def __init__(self, jobNum, taskLen, slack):
-		self.jobNum = jobNum
-		self.taskLen = taskLen
-		self.slack = slack
-
-
-	def getSlack(self):
-		return self.slack
-	def setSlack(self, slack):
-		self.slack = slack
-
-	def getJobNum(self):
-		return self.jobNum
-
-	def getTaskLen(self):
-		return self.taskLen
-
-
-#input: the job endtime, job number and capacity schedule
-# takes any jobs with same job number ans job end time and readjusts based on new job end time
-def changeSlackTimes(jobEndTime, jobNumber, CapacitySchedule):
+def scheduleListOfJobs(jobs):
+	numberOfJobs = len(jobs)
 	
-	for i in range(len(CapacitySchedule)):
+	#create empty capacity schedule
+	CapacitySched = []
+	for i in range(MAX_LEN_CAP_SCHEDULE):
+		timeSlot = TimeSlot()
+		CapacitySched.append(timeSlot)
 
-		if (CapacitySchedule[i].jobNum == jobNumber):
-			currentTaskEndTime = CapacitySchedule[i].taskLen + i 
-			slack = jobEndTime - currentTaskEndTime
+	#Assign initial job (first vector within vector)
+	CapacitySched = assignInitialJob(CapacitySched,jobs)
 
-			CapacitySchedule[i].setSlack(slack)
-
-#input: LIST OF TASKS (A SINGLE JOB)
-#return: the endtime for that job, and an initialized capacity schedule
-def assignInitialJob(job):
-	#print("not implemented yet")
-
-	CapacitySchedule = []
-
-	EndTime = job[0]
-
-	curSlack = 0
-	for i in range(len(job)):
-
-		#if the new task adds to the end time, reset end time and change all tasks' slack times
-		if (job[i] + i > EndTime):
-			#change end time
-			EndTime = job[i] + i
-
-			#call function to readjust slack times
-			changeSlackTimes(EndTime, 1, CapacitySchedule)
-
-			#ALSO NEED TO REASSIGN THE REST OF THE END TIMES FOR THE JOBS 
-
-		#CALCULATE SLACK
-		curSlack = EndTime - job[i] - i
-
-		#CREATE OBJECT TO ADD TO CAPACITY SCHEDULE
-		capAssignment = Time(1,job[i],curSlack)
-
-		#add to capacity schedule (assuming task lengths are in order)
-		CapacitySchedule.append(capAssignment)
-
-	return EndTime, CapacitySchedule
+	#Assign all other jobs (2nd to nth jobs where n is number of jobs)
+	CapacitySched = assignRestOfJobs(CapacitySched, jobs)
 
 
-def getJobEndTime(jobNum, CapacitySchedule):
+	#shift back any jobs that have empty time slots before them
+	shiftBack(CapacitySched)
 
-	maximum = 0
-	for i in range(len(CapacitySchedule)):
-
-		if (CapacitySchedule[i].jobNum == jobNum):
-
-			if (CapacitySchedule[i].taskLen + i > maximum):
-				maximum = CapacitySchedule[i].taskLen + i
-				
-
-	return maximum
-
-
-def increaseSlackForJob(jobNum, CapacitySchedule):
-	#print("not yet implemented yet")
-
-	for i in range(len(CapacitySchedule)):
-		if (CapacitySchedule[i].getJobNum() == jobNum):
-
-			currentSlack = CapacitySchedule[i].getSlack()
-			CapacitySchedule[i].setSlack(currentSlack + 1)
-
-
-
-#find the right spot to add the task 
-#return the index
-def findTaskSpot(jobNum, taskLen, CapacitySchedule):
-
+	curSumOfEndTimes = getSumOfEndTimes(numberOfJobs, CapacitySched)
 	
-	oldSched = CapacitySchedule.copy()
-	minIndex = len(CapacitySchedule)
+	modifySchedule = CapacitySched.copy()
 
 
-	##################################################################
-	##############  TRY FIRST JOB SPOT BY APPENDING TO THE END #######
-	time = Time(jobNum,taskLen,0)
-	CapacitySchedule.append(time)
+	adjustmentsToCapSched = 0
+	while(True):
 
-	EndTimes = []
-	curEndTime = 0
-	for j in range(jobNum):
+		dictList = createSlackDictionary(modifySchedule, numberOfJobs)
 
-		curEndTime = getJobEndTime(j+1, CapacitySchedule)
-		EndTimes.append(curEndTime)
+		newSchedule = readjustCapacitySchedule(modifySchedule, dictList)
 
+		newSumOfEndTimes = getSumOfEndTimes(numberOfJobs, newSchedule)
 
-	for j in range(jobNum):
-	
-		# I need an end Time vector calculation 
-		changeSlackTimes(EndTimes[j], j, CapacitySchedule)
-
-		for k in range(len(CapacitySchedule)):
-
-			while(CapacitySchedule[k].getSlack() < 0):
-				curJobNum = CapacitySchedule[k].getJobNum()
-
-				increaseSlackForJob(curJobNum, CapacitySchedule)
-	minSum = 0
-	for j in range(len(EndTimes)):
-		minSum = minSum + EndTimes[j]
-
-
-	CapacitySchedule = oldSched.copy()
-
-	##################################################################
-
-	##################################################################
-	############## 		TRY ALL OTHER SPOTS ##########################
-
-	for i in reversed(range(len(CapacitySchedule))):
-#	for i in range(len(CapacitySchedule)):
-
-		time = Time(jobNum, taskLen, 0)
+		if (newSumOfEndTimes == curSumOfEndTimes):
+			break
 		
-		#insert time object into index i
-		CapacitySchedule.insert(i, time)
+		adjustmentsToCapSched = adjustmentsToCapSched + 1
 
-		#printSlackValues(CapacitySchedule)
-		#add to Capacity Schedule
+		curSumOfEndTimes = newSumOfEndTimes
+		modifySchedule = newSchedule.copy()
 
-		EndTimes = []
-		curEndTime = 0
-		for j in range(jobNum):
-
-			curEndTime = getJobEndTime(j+1, CapacitySchedule)
-			EndTimes.append(curEndTime)
-
-
-		for j in range(jobNum):
+	return CapacitySched, newSchedule, adjustmentsToCapSched
 	
-			# I need an end Time vector calculation 
-			changeSlackTimes(EndTimes[j], j, CapacitySchedule)
-
-			for k in range(len(CapacitySchedule)):
-
-				while(CapacitySchedule[k].getSlack() < 0):
-					curJobNum = CapacitySchedule[k].getJobNum()
-
-					increaseSlackForJob(curJobNum, CapacitySchedule)
-
-
-		curSum = 0
-
-		for j in range(len(EndTimes)):
-			curSum = curSum + EndTimes[j]
-
-		if (curSum < minSum):
-			minSum = curSum
-			minIndex = i
-
-		"""
-		print("adjusted slack times: ")
-		printSlackValues(CapacitySchedule)
-
-		print("="*40)
-		print("Sum of end times: " + str(curSum))
-		print("="*40)
-		"""
-
-		CapacitySchedule = []
-		for k in range(len(oldSched)):
-			CapacitySchedule.append(oldSched[k])
-
-
-		CapacitySchedule = oldSched.copy()
-
-#	print(minIndex)
-	return minIndex, oldSched
-
-
-
-#add the task to the index
-
-def addTaskToIndex(index, jobNum, taskLen, CapacitySchedule):
-
-	time = Time(jobNum, taskLen, 0)
-	
-	#add Time object to capacity schedule
-	if (index == len(CapacitySchedule)):
-		CapacitySchedule.append(time)
-	else:
-		CapacitySchedule.insert(index,time)
-
-	#adjust slack times based on addition
-
-	MaxJob = 0
-	#get the total number of jobs that exist in the capacity schedule
-	for i in range(len(CapacitySchedule)):
-		if (CapacitySchedule[i].getJobNum() > MaxJob):
-			MaxJob = CapacitySchedule[i].getJobNum()
-
-	#calculate each job end time in the capacity schedule
-	EndTimes = []
-	curEndTime = 0
-	for i in range(MaxJob):
-	#def getJobEndTime(jobNum, CapacitySchedule):
-		curEndTime = getJobEndTime(i+1, CapacitySchedule)
-		EndTimes.append(curEndTime)
-
-
-
-	#adjust slack times based on end times vector
-	for i in range(MaxJob):
-		#def changeSlackTimes(jobEndTime, jobNumber, CapacitySchedule):
-		changeSlackTimes(EndTimes[i], i+1, CapacitySchedule)
-
-	#printSlackValues(CapacitySchedule)
-	#print("="*50)
-
-
-
-def assignAllJobs(jobList):
-	
-	intialEndTime = 0
-	CapSched = []
-
-	EndTimes = []
-
-	initialEndTime, CapSched = assignInitialJob(jobList[0])
-	EndTimes.append(initialEndTime)
-
-	oldSched = CapSched.copy()
-
-	#add all jobs in the job list:
-	for i in range(len(jobList)):
-		
-		#if it is the initial job, don't do anything (already initialized)
-		if (i == 0):
+#create a function to print the schedules
+#(if there's any difference, print old and new)
+#(if no difference only print one)
+def printCapacitySchedules(oldSched, newSched, numberOfJobs):
+	different = False
+	for i in range(len(oldSched)):
+		if (oldSched[i].assigned == False):
 			continue
+		old = oldSched[i].task
+		new = newSched[i].task
+		if not (old.getJobNum() == new.getJobNum() and old.getTaskLen() == new.getTaskLen()):
+			different = True
+	print("="*50)
+	if (different):
+	#Print out old schedule and new schedule (if no adjustments, old schedule should be the same as new schedule)
+		print("Old Schedule:")
+		printSlackValues(oldSched)
+		print("="*50)
+		printEndTimes(numberOfJobs, oldSched)
+		print("="*50)
+		print("="*50)
 		
-		#add all tasks in an individual job		
-		for j in range(len(jobList[i])):
-			
-			newSched = []
-			jobNum = i + 1
-			taskLen = jobList[i][j]
-	
-			#find what index is best place to put job
-			index, newSched = findTaskSpot(jobNum, taskLen, oldSched)
-			
-			#place job at optimal location found
-			addTaskToIndex(index, jobNum, taskLen, newSched)
-	
-			oldSched = newSched.copy()
-
-	return newSched
-
-
-#display the information regarding the created capacity schedule
-def printSlackValues(CapacitySchedule):
-	print()
-	for i in range(len(CapacitySchedule)):
-
-		print("t=" + str(i) + ": job num: " + str(CapacitySchedule[i].jobNum) + ", task len: " + str(CapacitySchedule[i].taskLen) +  ", slack: " + str(CapacitySchedule[i].slack))
-
-#Iterate through the EndTimes vector and print out values with the job numbers
-def printEndTimes(EndTimes):
-
-	print("End Times:")
-	for i in range(len(EndTimes)):
-		print("Job " + str(i+1) +": " + str(EndTimes[i]))
+		print("New Schedule:")
+		printSlackValues(newSched)
+		print("="*50)
+		printEndTimes(numberOfJobs, newSched)
+		#print("not implemented yet")
+	else:
+		print("Schedule: ")
+		printSlackValues(newSched)
+		print("="*50)
+		printEndTimes(numberOfJobs, newSched)
 
 if __name__ == "__main__":
 
 
-	job = [10,9,9,7,5]
-	#job2 = [10,8,6,4,2]
-	#job3 = [10,8,6,4,2]
+	
+	numberOfJobs = 25
+	nummberOfPathTasks = 3
+	minPathLength = 4
+	maxPathLength = 13
 
 	jobs = []
-	for i in range(3):
-		jobs.append(job)
-	
-
-
-	capacity = assignAllJobs(jobs)
-
-	printSlackValues(capacity)
-
-	numberOfJobs = len(jobs)
-
-	EndTimes = []
-	print("End Times: ")
+#	"""
 	for i in range(numberOfJobs):
+		curJob = []
+		for j in range(nummberOfPathTasks):
+			curTask = random.randint(minPathLength, maxPathLength)
+			curJob.append(curTask)
+		curJob.sort(reverse=True)
+		jobs.append(curJob)
 
-		curEndTime = getJobEndTime(i+1, capacity)
-		print("Job " + str(i+1) + ": " + str(curEndTime))
-		EndTimes.append(curEndTime)
+	
+	for i in range(len(jobs)):
+		print("Job " + str(i+1))
+		for j in range(len(jobs[i])):
+			print(str(jobs[i][j]) + " ", end='')
+		print("\n")
+#	"""
 
+
+
+	oldSchedule, newSchedule, numberOfAdjustments = scheduleListOfJobs(jobs)
+
+	printCapacitySchedules(oldSchedule, newSchedule, numberOfJobs)
+	print("="*50)
+	print("Number of Adjustments: " + str(numberOfAdjustments))
